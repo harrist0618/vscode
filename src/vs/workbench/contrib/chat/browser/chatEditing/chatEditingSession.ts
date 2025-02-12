52132fe12bd49d9c8bd9ab07305103637886c6bd
Don Jayamanne <don.jayamanne@outlook.com>
2025-02-13 10:18:05 +1100

    Add Chat NotebookEdit API

--- src/vs/workbench/contrib/chat/browser/chatEditing/chatEditingSession.ts
+++ src/vs/workbench/contrib/chat/browser/chatEditing/chatEditingSession.ts
@@ -41,7 +41,7 @@ import { isNotebookEditorInput } from '../../../notebook/common/notebookEditorIn
 import { INotebookService } from '../../../notebook/common/notebookService.js';
 import { ChatEditingSessionChangeType, ChatEditingSessionState, ChatEditKind, getMultiDiffSourceUri, IChatEditingSession, IModifiedFileEntry, WorkingSetDisplayMetadata, WorkingSetEntryRemovalReason, WorkingSetEntryState } from '../../common/chatEditingService.js';
 import { IChatResponseModel } from '../../common/chatModel.js';
-import { IChatService } from '../../common/chatService.js';
+import { ICellEditReplaceOperation, IChatService } from '../../common/chatService.js';
 import { ChatEditingModifiedFileEntry, IModifiedEntryTelemetryInfo, ISnapshotEntry } from './chatEditingModifiedFileEntry.js';
 import { ChatEditingModifiedNotebookEntry } from './chatEditingModifiedNotebookEntry.js';
 import { ChatEditingTextModelContentProvider } from './chatEditingTextModelContentProviders.js';
@@ -595,6 +595,9 @@ export class ChatEditingSession extends Disposable implements IChatEditingSessio
 
 		// ensure that the edits are processed sequentially
 		this._sequencer.queue(() => this._acceptTextEdits(resource, textEdits, isLastEdits, responseModel));
+	}
+	acceptNotebookEdits(_resource: URI, _edits: ICellEditReplaceOperation[], _isLastEdits: boolean, _responseModel: IChatResponseModel): void {
+		//
 	}
 
 	resolve(): void {
